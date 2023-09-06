49306a9796aeaa0166aff6a839f3949fcce1f5d4
thales <thalesgoncalves3@gmail.com>
2023-09-06 13:29:13 -0300

    teste hosting

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -28,116 +28,119 @@ class _LoginPageState extends State<LoginPage> {
         child: Form(
           key: formKey,
           autovalidateMode: AutovalidateMode.disabled,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: email,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Email",
-                    labelText: "Email"),
-              ),
-              TextFormField(
-                controller: password,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Password",
-                    labelText: "Password"),
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    if (formKey.currentState!.validate()) {
-                      final login =
-                          await AuthController.login(email.text, password.text);
+          child: Center(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Email",
+                      labelText: "Email"),
+                ),
+                TextFormField(
+                  controller: password,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Password",
+                      labelText: "Password"),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()) {
+                        final login = await AuthController.login(
+                            email.text, password.text);
 
-                      if (login) {
-                        QuerySnapshot query =
-                            await DatabaseController.getUser();
-                        List<QueryDocumentSnapshot> documents = query.docs;
-                        var teste = documents[0].data() as Map<String, dynamic>;
-                        if (teste["partner"]) {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const PartnerPage(),
-                              ));
+                        if (login) {
+                          QuerySnapshot query =
+                              await DatabaseController.getUser();
+                          List<QueryDocumentSnapshot> documents = query.docs;
+                          var teste =
+                              documents[0].data() as Map<String, dynamic>;
+                          if (teste["partner"]) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const PartnerPage(),
+                                ));
+                          } else {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const UserPage(),
+                                ));
+                          }
                         } else {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const UserPage(),
-                              ));
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text("Email ou senha errado(s)"),
+                                content: const Text("Não foi possivel logar"),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        email.clear();
+                                        password.clear();
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text("OK"))
+                                ],
+                              );
+                            },
+                          );
                         }
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text("Email ou senha errado(s)"),
-                              content: const Text("Não foi possivel logar"),
-                              actions: [
-                                TextButton(
-                                    onPressed: () {
-                                      email.clear();
-                                      password.clear();
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text("OK"))
-                              ],
-                            );
-                          },
-                        );
                       }
-                    }
+                    },
+                    child: const Text("Entrar")),
+                ElevatedButton(
+                  onPressed: () async {
+                    // final FirebaseAuth _auth = FirebaseAuth.instance;
+                    // final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+                    // Future<User?> signInWithGoogle() async {
+                    //   try {
+                    //     final GoogleSignInAccount? googleSignInAccount =
+                    //         await _googleSignIn.signIn();
+
+                    //     if (googleSignInAccount != null) {
+                    //       final GoogleSignInAuthentication
+                    //           googleSignInAuthentication =
+                    //           await googleSignInAccount.authentication;
+
+                    //       final AuthCredential credential =
+                    //           GoogleAuthProvider.credential(
+                    //         accessToken: googleSignInAuthentication.accessToken,
+                    //         idToken: googleSignInAuthentication.idToken,
+                    //       );
+
+                    //       final UserCredential authResult =
+                    //           await _auth.signInWithCredential(credential);
+                    //       final User? user = authResult.user;
+                    //       print(user);
+                    //       return user;
+                    //     }
+
+                    //     return null;
+                    //   } catch (e) {
+                    //     print(e.toString());
+                    //     return null;
+                    //   }
+                    // }
                   },
-                  child: const Text("Entrar")),
-              ElevatedButton(
-                onPressed: () async {
-                  // final FirebaseAuth _auth = FirebaseAuth.instance;
-                  // final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-                  // Future<User?> signInWithGoogle() async {
-                  //   try {
-                  //     final GoogleSignInAccount? googleSignInAccount =
-                  //         await _googleSignIn.signIn();
-
-                  //     if (googleSignInAccount != null) {
-                  //       final GoogleSignInAuthentication
-                  //           googleSignInAuthentication =
-                  //           await googleSignInAccount.authentication;
-
-                  //       final AuthCredential credential =
-                  //           GoogleAuthProvider.credential(
-                  //         accessToken: googleSignInAuthentication.accessToken,
-                  //         idToken: googleSignInAuthentication.idToken,
-                  //       );
-
-                  //       final UserCredential authResult =
-                  //           await _auth.signInWithCredential(credential);
-                  //       final User? user = authResult.user;
-                  //       print(user);
-                  //       return user;
-                  //     }
-
-                  //     return null;
-                  //   } catch (e) {
-                  //     print(e.toString());
-                  //     return null;
-                  //   }
-                  // }
-                },
-                child: const Text("Entrar com o Google"),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterPage(),
-                        ));
-                  },
-                  child: Text("Cadastrar"))
-            ],
+                  child: const Text("Entrar com o Google"),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ));
+                    },
+                    child: Text("Cadastrar"))
+              ],
+            ),
           ),
         ),
       ),
